dcbccf013d01fc804743c8bc491f6eb87e8ef2b3
Skumar879 <skumar879@student.fuhsd.org>
2019-03-27 11:02:51 -0700

    new stuff 11:02 3/27/19

--- src/Graph.java
+++ src/Graph.java
@@ -4,6 +4,59 @@ public class Graph {
     private ArrayList<Node> nodes;
 
     public Graph(){
+        nodes = new ArrayList<>();
+    }
 
+    public void addNode(String name) {
+    }
+
+    public void addDirectedEdge(String name1, String name2) {
+        Node n1 = /*get Node for name 1*/;
+        Node n2 = /*get Node for name 2*/;
+        n1.addNeighbor(n2);
+    }
+
+    public void addUndirectedEdge(String name1, String name2) {
+    }
+
+    public Node getNode(String name) {
+    }
+
+    public class Node{
+        private String name;
+        private ArrayList<Node> neighbors;
+
+        private Node(String name){
+            neighbors = new ArrayList<Node>();
+            this.name = name;
+        }
+
+        private void addNeighbor(Node n){
+            //add this node as a neighbor
+        }
+
+        /**
+         * Returns a String of the names of all the neighbors of this node
+         * @return
+         */
+        public String getNeighborNames(){
+            return " ";
+        }
+
+        /**
+         * Return neighbor whose name is name. Returns null otherwise.
+         * @param name name of neighboring node to return
+         * @return returns neighboring node with correct name
+         */
+        public Node getNeighbor(String name){
+            return null;
+        }
+        public Node getNode(Node j){
+            return null;
+        }
+
+        public String getName(){
+            return name;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,68 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Build up a graph of connected nodes
-        Node root = new Node("hall");
+        Graph g = new Graph();
+        g.addNode("hall");
+        g.addNode("closet");
+        g.addNode("dungeon");
 
-        root.addNeighbor(new Node ("closet"));
-        root.addNeighbor(new Node ("bedroom"));
-
-        root.getNeighbor("closet").addNeighbor(new Node("hall"));
-
-        //"game loop"  where I get user input and move the player.
-
-        Node currentRoom = root;
+        g.addDirectedEdge("hall", "dungeon");
+        g.addUndirectedEdge("hall", "closet");
+        Graph.Node current = g.getNode("hall");
 
         String response = "";
-        Scanner in = new Scanner(System.in);
-
+        Scanner s = new Scanner(System.in);
         do{
             //display the room and the exits
-            System.out.println("You are currently in the " + currentRoom.getName());
-            System.out.println("You can go to the: " + currentRoom.getNeighborNames());
+            System.out.println("You are currently in the " + current.getName());
+            System.out.println("What do you want to do? >");
+            response = s.nextLine();
+            if (response.substring(0, 2).equals("go")){
+                String room = response.substring()
+            } else if (){
 
-            //ask player what they want to do
-            System.out.print("Type the name of the room you want to go to: ");
-            response = in.nextLine();
+            } else if () {
 
-            //do that
-            Node nextRoom = currentRoom.getNeighbor(response);
-            if(nextRoom == null) {
-                System.out.println("You can't go to " + response + " try again");
+            } else if (){
+
             } else{
-                currentRoom = nextRoom;
+
             }
 
         } while (!response.equals("quit"));
+
+//        //Build up a graph of connected nodes
+//        Node root = new Node("hall");
+//
+//        root.addNeighbor(new Node ("closet"));
+//        root.addNeighbor(new Node ("bedroom"));
+//
+//        root.getNeighbor("closet").addNeighbor(new Node("hall"));
+//
+//        //"game loop"  where I get user input and move the player.
+//
+//        Node currentRoom = root;
+//
+//        String response = "";
+//        Scanner in = new Scanner(System.in);
+//
+//        do{
+//            //display the room and the exits
+//            System.out.println("You are currently in the " + currentRoom.getName());
+//            System.out.println("You can go to the: " + currentRoom.getNeighborNames());
+//
+//            //ask player what they want to do
+//            System.out.print("Type the name of the room you want to go to: ");
+//            response = in.nextLine();
+//
+//            //do that
+//            Node nextRoom = currentRoom.getNeighbor(response);
+//            if(nextRoom == null) {
+//                System.out.println("You can't go to " + response + " try again");
+//            } else{
+//                currentRoom = nextRoom;
+//            }
+//
+//        } while (!response.equals("quit"));
     }
 }
